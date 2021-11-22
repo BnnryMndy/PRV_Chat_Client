6a354893449def3812e1425538304ba67c989eeb
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-11-22 13:45:29 +0300

    Now it's works

--- PRV_Chat_Client/Form1.Designer.cs
+++ PRV_Chat_Client/Form1.Designer.cs
@@ -29,67 +29,54 @@ namespace PRV_Chat_Client
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SendMessageButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // SendMessageButton
             // 
             this.SendMessageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SendMessageButton.Location = new System.Drawing.Point(734, 485);
+            this.SendMessageButton.Location = new System.Drawing.Point(328, 479);
             this.SendMessageButton.Name = "SendMessageButton";
             this.SendMessageButton.Size = new System.Drawing.Size(54, 51);
             this.SendMessageButton.TabIndex = 1;
-            this.SendMessageButton.Text = "◹";
+            this.SendMessageButton.Text = "→";
             this.SendMessageButton.UseVisualStyleBackColor = true;
             this.SendMessageButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // inputTextBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(12, 485);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(716, 48);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(71, 17);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 26);
-            this.textBox2.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "логин";
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 479);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(305, 48);
+            this.inputTextBox.TabIndex = 2;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(12, 49);
+            this.listBox1.Location = new System.Drawing.Point(12, 9);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(365, 424);
+            this.listBox1.Size = new System.Drawing.Size(365, 464);
             this.listBox1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 548);
+            this.ClientSize = new System.Drawing.Size(394, 548);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.SendMessageButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -101,10 +88,9 @@ namespace PRV_Chat_Client
 
         #endregion
         private System.Windows.Forms.Button SendMessageButton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PRV_Chat_Client/Form1.cs
+++ PRV_Chat_Client/Form1.cs
@@ -9,27 +9,68 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.IO.Pipes;
+using System.Threading;
 
 namespace PRV_Chat_Client
 {
     public partial class Form1 : Form
     {
         NamedPipeClientStream cs;
+        StreamReader sr;
+        StreamWriter sw;
+        Task<string> task;
+
         public Form1()
         {
             InitializeComponent();
+
+            cs = new NamedPipeClientStream(".", "server", PipeDirection.InOut, PipeOptions.Asynchronous);
+            cs.Connect();
+            
+            sr = new StreamReader(cs);
+            sw = new StreamWriter(cs);
+            task = ReadLineAsync(sr);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            cs = new NamedPipeClientStream(".", "server", PipeDirection.InOut);
-            cs.Connect();
-            StreamReader sr = new StreamReader(cs);
-            StreamWriter sw = new StreamWriter(cs);
-            sw.WriteLine("сервер");
+            sw.WriteLine(inputTextBox.Text);
             sw.Flush();
-            string inp = sr.ReadLine();
-            //responseLabel.Text = inp;
+            inputTextBox.Text = "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (task.IsCompleted || task.IsFaulted || task.IsCanceled)
+            {
+                task = ReadLineAsync(sr);
+            }
+
+        }
+
+        private async Task<string> ReadLineAsync(StreamReader sr)
+        {
+
+            string st = await sr.ReadLineAsync();
+            if (!string.IsNullOrEmpty(st))
+            {
+               AddMsgToList(st);
+            }
+
+            return st;
+        }
+
+        private void AddMsgToList(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                Action<string> tmp = this.AddMsgToList;
+                this.Invoke(tmp, msg);
+                return;
+            }
+            listBox1.Items.Add(msg);
+
         }
     }
 }
